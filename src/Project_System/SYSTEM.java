a451a3e4d184910fec613befff060a45ba6c4a5a
Adrian <Adrian@WIN-RO2UQQ0R3FN>
2026-04-18 14:33:39 +0800

    dsds

---
+++ src/Project_System/SYSTEM.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project_System;
+
+/**
+ *
+ * @author Adrian
+ */
+public class SYSTEM {
+        public static void main (String[] args){
+            LOGIN login = new LOGIN();
+            login.setVisible(true);
+            login.pack();
+            login.setLocationRelativeTo(null);
+        }
+}
